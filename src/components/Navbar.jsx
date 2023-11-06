7d648a66563d43a0c88e0f9472d9824bf18e6d92
GoneAway <vasil.zisis@gmail.com>
2023-11-06 21:46:43 +0200

    navbar is responsive now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,43 @@ import { useState } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+  const [toggleOpen, setToggleOpen] = useState(false)
+
+  const handleOpen = e => {
+    e.stopPropagation()
+    setToggleOpen(!toggleOpen)
+    if (!toggleOpen) {
+      e.currentTarget.classList.add('active')
+      e.currentTarget.setAttribute('aria-expanded', 'true')
+    } else {
+      e.currentTarget.classList.remove('active')
+      e.currentTarget.setAttribute('aria-expanded', 'false')
+    }
+  }
+
   return (
     <header>
       <nav>
         <div className='nav-img-div'>
-          <img
-            className='nav-img'
-            src='https://res.cloudinary.com/dmdbza74n/image/upload/v1632158213/studiophotocreation/logo-black_uw3ade.png'
-          />
+          <NavLink to='/'>
+            <img
+              className='nav-img'
+              src='https://res.cloudinary.com/dmdbza74n/image/upload/v1632158213/studiophotocreation/logo-black_uw3ade.png'
+            />
+          </NavLink>
+          <button
+            aria-expanded='false'
+            className='hamburger'
+            onClick={e => handleOpen(e)}
+          >
+            <span className='bar' aria-hidden='true'></span>
+            <span className='bar' aria-hidden='true'></span>
+            <span className='bar' aria-hidden='true'></span>
+            <span className='screen-reader-text'>Menu</span>
+          </button>
         </div>
-        <div className='nav-ul-div'>
-          <ul className='nav-ul'>
+        <div>
+          <ul className={toggleOpen ? 'nav-ul-mobile' : 'nav-ul-desktop'}>
             <li className='nav-li'>
               <NavLink className='navlink' to='/'>
                 Cypriot Weddings
