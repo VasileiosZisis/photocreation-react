25366b658088c3377dfd6926c7c86a3ffe4fef54
GoneAway <vasil.zisis@gmail.com>
2023-11-08 22:51:30 +0200

    categories section is responsive

--- src/components/CategoryContainer.jsx
+++ src/components/CategoryContainer.jsx
@@ -1,6 +1,13 @@
 import './CategoryContainer.css'
 
-const CategoryContainer = ({ width, height, top, left, title, src }) => {
+const CategoryContainer = ({
+  width,
+  height,
+  top = 0,
+  left = 0,
+  title,
+  src
+}) => {
   const aStyle = {
     position: 'absolute',
     top: top,
@@ -15,20 +22,36 @@ const CategoryContainer = ({ width, height, top, left, title, src }) => {
   }
   return (
     <>
-      <h3 style={h3Size} className='categories-h3'>
-        {title}
-      </h3>
-      <a href='/' style={aStyle} className='categories-a'>
-        <div className='categories-div'>
-          <p className='categories-p'>VIEW ALL</p>
-          <img
-            className='categories-img'
-            src={src}
-            alt=''
-            role='presentation'
-          />
-        </div>
-      </a>
+      <div className='desktop'>
+        <h3 style={h3Size} className='categories-h3'>
+          {title}
+        </h3>
+        <a href='/' style={aStyle} className='categories-a'>
+          <div className='categories-div'>
+            <p className='categories-p'>VIEW ALL</p>
+            <img
+              className='categories-img'
+              src={src}
+              alt=''
+              role='presentation'
+            />
+          </div>
+        </a>
+      </div>
+      <div className='mobile'>
+        <h3 className='categories-h3'>{title}</h3>
+        <a href='/' className='categories-a'>
+          <div className='categories-div'>
+            <p className='categories-p'>VIEW ALL</p>
+            <img
+              className='categories-img'
+              src={src}
+              alt=''
+              role='presentation'
+            />
+          </div>
+        </a>
+      </div>
     </>
   )
 }
